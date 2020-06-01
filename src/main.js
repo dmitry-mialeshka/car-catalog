067020ede4949c22b16aad46b15c5d3705ede4c7
Dmitry Mialeshka <dmitry.mialeshka@intexsoft.by>
2020-06-01 16:07:02 +0300

    Added bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
